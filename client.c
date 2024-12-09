58a1bc38a673d414de64dd1418f10daf67969a6e
Kazuki Adachi <kadachi@student.42tokyo.jp>
2024-12-09 12:41:31 +0900

    [FIX] fluent talk without fixed usleep()

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: kadachi <kadachi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 20:36:58 by kadachi           #+#    #+#             */
-/*   Updated: 2024/12/07 19:15:33 by kadachi          ###   ########.fr       */
+/*   Updated: 2024/12/08 20:24:57 by kadachi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,17 @@
 #include <unistd.h>
 #include "libft.h"
 
+volatile sig_atomic_t	g_flag = 0b00;
+
 static void	sigusr_handler(int signum)
 {
-	(void)signum;
+	if (signum == SIGUSR1)
+		g_flag &= ~0b01;
 }
 
-static void	exit_with_msg(char *msg)
+static void	exit_on_error(char *msg)
 {
-	ft_putstr_fd(msg, 2);
+	ft_putstr_fd(msg, STDERR_FILENO);
 	exit(EXIT_FAILURE);
 }
 
@@ -33,18 +36,19 @@ static void	send_byte(int pid, char c)
 	i = 0;
 	while (i++ < 8)
 	{
-		if (c & 0x01)
+		g_flag |= 0b01;
+		if (c & 0b00000001)
 		{
 			if (kill(pid, SIGUSR1) == -1)
-				exit_with_msg("[ERR] kill() failed.\n");
+				exit_on_error("[ERR] kill() failed.\n");
 		}
 		else
 		{
 			if (kill(pid, SIGUSR2) == -1)
-				exit_with_msg("[ERR] kill() failed.\n");
+				exit_on_error("[ERR] kill() failed.\n");
 		}
-		pause();
-		ft_putstr_fd(".", 1);
+		while (g_flag & 0b01)
+			usleep(1);
 		c >>= 1;
 	}
 }
@@ -56,21 +60,18 @@ int	main(int argc, char **argv)
 	int					i;
 
 	if (argc != 3)
-		exit_with_msg("Usage: ./client {PID} {msg}\n");
+		exit_on_error("Usage: ./client {PID} {msg}\n");
 	pid = ft_atoi(argv[1]);
 	if (pid <= 0)
-		exit_with_msg("Usage: ./client {PID} {msg}\n");
+		exit_on_error("Usage: ./client {PID} {msg}\n");
 	sigact.sa_handler = sigusr_handler;
 	sigact.sa_flags = 0;
 	sigemptyset(&sigact.sa_mask);
-	if (sigaction(SIGUSR1, &sigact, NULL) == 1)
-		exit_with_msg("[ERR] sigaction() failed.\n");
+	if (sigaction(SIGUSR1, &sigact, NULL) == -1)
+		exit_on_error("[ERR] sigaction() failed.\n");
 	i = 0;
 	while (argv[2][i] != '\0')
-	{
 		send_byte(pid, argv[2][i++]);
-		ft_putstr_fd("sent\n", 1);
-	}
 	send_byte(pid, '\0');
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,40 +6,41 @@
 /*   By: kadachi <kadachi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 20:37:04 by kadachi           #+#    #+#             */
-/*   Updated: 2024/12/07 19:23:46 by kadachi          ###   ########.fr       */
+/*   Updated: 2024/12/08 20:25:16 by kadachi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <unistd.h>
 #include "libft.h"
 
 void	sigusr_handler(int signum, siginfo_t *siginfo, void *ctx)
 {
 	static char	c;
 	static int	i;
+	char		nl;
 
+	nl = '\n';
 	(void)ctx;
 	if (signum == SIGUSR1)
-		c |= 1 << i;
+		c |= 0b000000001 << i;
 	i++;
 	if (i == 8)
 	{
 		if (c == '\0')
-			ft_putchar_fd('\n', 1);
+			ft_putchar_fd(nl, STDOUT_FILENO);
 		else
-			ft_putchar_fd(c, 1);
+			ft_putchar_fd(c, STDOUT_FILENO);
 		c = 0;
 		i = 0;
 	}
-	usleep(100);
 	kill(siginfo->si_pid, SIGUSR1);
 }
 
-static void	exit_with_msg(char *msg)
+static void	exit_on_error(char *msg)
 {
-	ft_putstr_fd(msg, 2);
+	ft_putstr_fd(msg, STDERR_FILENO);
 	exit(1);
 }
 
@@ -47,18 +48,16 @@ int	main(void)
 {
 	struct sigaction	sigact;
 
-	ft_putstr_fd("Server PID: ", 1);
-	ft_putnbr_fd(getpid(), 1);
-	ft_putchar_fd('\n', 1);
+	ft_putstr_fd("Server PID: ", STDOUT_FILENO);
+	ft_putnbr_fd(getpid(), STDOUT_FILENO);
+	ft_putchar_fd('\n', STDOUT_FILENO);
 	sigact.sa_sigaction = sigusr_handler;
 	sigact.sa_flags = SA_SIGINFO;
 	sigemptyset(&sigact.sa_mask);
-	sigaddset(&sigact.sa_mask, SIGUSR1);
-	sigaddset(&sigact.sa_mask, SIGUSR2);
 	if (sigaction(SIGUSR1, &sigact, NULL) == -1)
-		exit_with_msg("[ERR] sigaction(SIGUSR1, ...) failed.\n");
+		exit_on_error("[ERR] sigaction(SIGUSR1, ...) failed.\n");
 	if (sigaction(SIGUSR2, &sigact, NULL) == -1)
-		exit_with_msg("[ERR] sigaction(SIGUSR2, ...) failed.\n");
+		exit_on_error("[ERR] sigaction(SIGUSR2, ...) failed.\n");
 	while (1)
 		pause();
 	return (0);
